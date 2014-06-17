e8e8f5020d12c4f9baec18130f30c9b84a0f7a54
hans ewetz <hansewetz@hotmail.com>
2014-06-17 14:46:34 +0200

    ewetzha: added stuff to translation request

--- src/cpp/libs/xlate-jobs/include/TranslateRequest.h
+++ src/cpp/libs/xlate-jobs/include/TranslateRequest.h
@@ -9,8 +9,8 @@ namespace xlate{
 class TranslateRequest{
 public:
   // ctors (movable, not copyable)
-  TranslateRequest(std::vector<std::string>const&segs);
-  TranslateRequest(std::initializer_list<std::string>segs);
+  TranslateRequest(std::string const&slan,std::string const&tlan,std::vector<std::string>const&segs);
+  TranslateRequest(std::string const&slan,std::string const&tlan,std::initializer_list<std::string>segs);
   TranslateRequest(TranslateRequest const&)=delete;
   TranslateRequest(TranslateRequest&&)=default;
   TranslateRequest&operator=(TranslateRequest&&)=default;
@@ -18,12 +18,16 @@ public:
 
   // getters
   std::string id()const;
+  std::string srcLan()const;
+  std::string targLan()const;
 
   // print function
   std::ostream&print(std::ostream&os)const;
 private:
-  std::vector<std::string>segs_;
   std::string id_;
+  std::vector<std::string>segs_;
+  std::string slan_;
+  std::string tlan_;
 };
 // print function
 std::ostream&operator<<(std::ostream&os,TranslateRequest const&);
--- src/cpp/libs/xlate-jobs/src/TranslateRequest.cc
+++ src/cpp/libs/xlate-jobs/src/TranslateRequest.cc
@@ -1,16 +1,33 @@
 #include "xlate-jobs/TranslateRequest.h"
+#include "utils/utility.h"
 #include <iostream>
 using namespace std;
 namespace xlate{
 
 // ctor
-TranslateRequest::TranslateRequest(vector<string>const&segs):segs_(segs){
+TranslateRequest::TranslateRequest(string const&slan,string const&tlan,vector<string>const&segs):
+  slan_(slan),tlan_(tlan),id_(utils::generateUuid()),segs_(segs){
 }
 // ctor
-TranslateRequest::TranslateRequest(initializer_list<string>segs):segs_(segs.begin(),segs.end()){
+TranslateRequest::TranslateRequest(string const&slan,string const&tlan,initializer_list<string>segs):
+  slan_(slan),tlan_(tlan),segs_(segs.begin(),segs.end()),id_(utils::generateUuid()){
+}
+// getters
+string TranslateRequest::id()const{
+  return id_;
+}
+// get source language
+string TranslateRequest::srcLan()const{
+  return slan_;
+}
+// get target language
+string TranslateRequest::targLan()const{
+  return tlan_;
 }
 // print function
 ostream&TranslateRequest::print(ostream&os)const{
+  os<<"id: "<<id_<<"srclan: "<<slan_<<", targetlan: "<<tlan_<<endl;
+  os<<"segs "<<endl;
   for(auto const&s:segs_)os<<s<<endl;
   return os;
 }
