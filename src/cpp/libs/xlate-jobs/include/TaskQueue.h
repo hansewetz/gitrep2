1ffd044e393730625191a90d8689f13cd1b0171d
hans ewetz <hansewetz@hotmail.com>
2014-06-19 08:01:59 +0200

    ewetzha: added TaskQueue

---
+++ src/cpp/libs/xlate-jobs/include/TaskQueue.h
@@ -0,0 +1,39 @@
+#ifndef __TASK_QUEUE_H__
+#define __TASK_QUEUE_H__
+#include <iosfwd>
+#include <memory>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+namespace xlate{
+
+// forward decl
+class TranslationTask;
+
+// queue holding shared pointers to TranslationTask objects
+class TaskQueue{
+public:
+  // ctors, dtor
+  TaskQueue(std::size_t maxsize);
+  TaskQueue(TaskQueue const&)=delete;
+  TaskQueue(TaskQueue&&)=default;
+  TaskQueue&operator=(TaskQueue&&)=default;
+  ~TaskQueue()=default;
+
+  // operations on queue
+  std::size_t size()const ;
+  bool enq(std::shared_ptr<TranslationTask>task);
+  std::shared_ptr<TranslationTask>deq(bool block);
+
+  // print function
+  std::ostream&print(std::ostream&os)const;
+private:
+  std::queue<std::shared_ptr<TranslationTask>>queue_;
+  mutable std::mutex mtx_;
+  mutable std::condition_variable cond_;
+  std::size_t maxsize_;
+};
+// debug print operator
+std::ostream&operator<<(std::ostream&os,TaskQueue const&t);
+}
+#endif
---
+++ src/cpp/libs/xlate-jobs/src/TaskQueue.cc
@@ -0,0 +1,48 @@
+#include "xlate-jobs/TaskQueue.h"
+#include "xlate-jobs/TranslationTask.h"
+#include <iostream>
+using namespace std;
+namespace xlate{
+
+// ctor
+TaskQueue::TaskQueue(size_t maxsize):maxsize_(maxsize){
+}
+// get #of items in queue
+size_t TaskQueue::size()const {
+  lock_guard<mutex>lock(mtx_);
+  return queue_.size();
+}
+// enqueue an item
+bool TaskQueue::enq(shared_ptr<TranslationTask>task){
+  {
+    lock_guard<mutex>lock(mtx_);
+    if(queue_.size()>=maxsize_)return false;
+    queue_.push(task);
+  }
+  cond_.notify_all();
+  return true;
+}
+// dequeue an item (block if needed)
+shared_ptr<TranslationTask>TaskQueue::TaskQueue::deq(bool block){
+  // wait blocking or wait for 0 duration
+  unique_lock<mutex>lock(mtx_);
+  if(block)cond_.wait(lock,[&](){return !queue_.empty();});
+  else cond_.wait_for(lock,chrono::milliseconds(0),[&](){return !queue_.empty();});
+
+  // if queue empty return null ptr
+  if(queue_.empty())return shared_ptr<TranslationTask>(nullptr);
+
+  // check if we have a message
+  shared_ptr<TranslationTask>ret{queue_.front()};
+  queue_.pop();
+  return ret;
+}
+// print function
+ostream&TaskQueue::print(ostream&os)const{
+   return os<<"#items: "<<size();
+}
+// debug print operator
+ostream&operator<<(ostream&os,TaskQueue const&t){
+  return t.print(os);
+}
+}
