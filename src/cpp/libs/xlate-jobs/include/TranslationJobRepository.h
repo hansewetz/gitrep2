5c63daa895ba2f53c1beffafdfdebe61c299d646
hans ewetz <hansewetz@hotmail.com>
2014-06-24 10:14:45 +0200

    ewetzha: modified job repository to only contains jobs or a specific language pair

--- src/cpp/libs/xlate-jobs/include/TranslationJobRepository.h
+++ src/cpp/libs/xlate-jobs/include/TranslationJobRepository.h
@@ -3,10 +3,7 @@
 #include "xlate-jobs/LanguageCode.h"
 #include "xlate-jobs/Identifiers.h"
 #include <iosfwd>
-#include <map>
-#include <vector>
 #include <memory>
-#include <queue>
 #include <mutex>
 #include <condition_variable>
 namespace xlate{
@@ -18,32 +15,26 @@ class TranslationJob;
 class TranslationJobRepository{
 public:
   // ctors, dtor
-  TranslationJobRepository();
+  TranslationJobRepository(LanguagePair const&lp);
   TranslationJobRepository(TranslationJobRepository const&)=delete;
   TranslationJobRepository(TranslationJobRepository&&)=default;
   TranslationJobRepository&operator=(TranslationJobRepository const&)=delete;
   TranslationJobRepository&operator=(TranslationJobRepository&&)=default;
   ~TranslationJobRepository()=default;
 
-  // repository query functions
-  std::size_t jobCount(LanguagePair const&)const;
-
   // repository update functions
   void addJob(std::shared_ptr<TranslationJob>);
-  std::shared_ptr<TranslationJob>getJobForTranslation(TranslationJobId const&);
-  std::shared_ptr<TranslationJob>removeJob(TranslationJobId const&);
+  std::shared_ptr<TranslationJob>getJobForTranslation();
 
   // print function
   std::ostream&print(std::ostream&os)const;
 private:
-  // store jobs in a set of queues indexed by LanguagePair
-  std::multimap<LanguagePair,std::queue<std::shared_ptr<TranslationJob>>>lanp2jobmap_;
-
   // map of job ids to jobs (we ensure jobids are unique)
-  std::map<TranslationJobId,std::shared_ptr<TranslationJob>>allJobs_;
-  std::map<TranslationJobId,std::shared_ptr<TranslationJob>>startedJobs_;
+  std::list<std::shared_ptr<TranslationJob>>waitingJobs_;
+  std::list<std::shared_ptr<TranslationJob>>processingJobs_;
 
-// NOTE! Must update 'jobsInTranslation_'
+  // language pair for this rep
+  LanguagePair lp_;
 
   // we have multiple consumers/producuerproducers
   mutable std::mutex mtx_;
--- src/cpp/libs/xlate-jobs/src/TranslationJobRepository.cc
+++ src/cpp/libs/xlate-jobs/src/TranslationJobRepository.cc
@@ -7,52 +7,31 @@ using namespace std;
 namespace xlate{
 
 // ctor
-TranslationJobRepository::TranslationJobRepository(){
-}
-// get #of jobs for a language pair
-size_t TranslationJobRepository::jobCount(LanguagePair const&lp)const{
-  lock_guard<mutex>lock(mtx_);
-  return lanp2jobmap_.count(lp);
+TranslationJobRepository::TranslationJobRepository(LanguagePair const&lp):lp_(lp){
 }
 // repository update functions
 void TranslationJobRepository::addJob(shared_ptr<TranslationJob>job){
+  // insert job in jobsPending_
   unique_lock<mutex>lock(mtx_);
-
-  // insert job in 'allJob map
-  allJobs_.insert(make_pair(job->id(),job));
-
-  // insert jobs in language pair quuee (if key is already in map, no insert is done)
-  auto it=lanp2jobmap_.find(job->lanpair());
-  if(it!=lanp2jobmap_.end()){
-    // insert new language pair queue
-    auto it=lanp2jobmap_.insert(make_pair(job->lanpair(),queue<shared_ptr<TranslationJob>>()));
-  }
-  queue<shared_ptr<TranslationJob>>&q=it->second;
-  q.push(job);
-
-  // notify waiing consumers
+  waitingJobs_.push_back(job);
   cond_.notify_all();
 }
 // get job for translation
-shared_ptr<TranslationJob>TranslationJobRepository::getJobForTranslation(TranslationJobId const&jobid){
-  // move job form allJobs --> startedJobs
-  lock_guard<mutex>lock(mtx_);
-  auto it=allJobs_.find(jobid);
-  if(it==allJobs_.end())THROW_RUNTIME("attempt to lookup job: "<<jobid<<" for translation");
-  startedJobs_.insert(*it);
-  allJobs_.erase(it);
-}
-// remove job and get job objects
-shared_ptr<TranslationJob>TranslationJobRepository::removeJob(TranslationJobId const&){
-  lock_guard<mutex>lock(mtx_);
-  // NOTE! Not yet done
-  // must remove to all 3 maps
-  cond_.notify_all();
+shared_ptr<TranslationJob>TranslationJobRepository::getJobForTranslation(){
+  // block until we have a job
+  unique_lock<mutex>lock(mtx_);
+  cond_.wait(lock,[&](){return !waitingJobs_.empty();});
+
+  // move job from waitingJobs_ --> processingJobs_ and return job
+  shared_ptr<TranslationJob>job{waitingJobs_.front()};
+  waitingJobs_.pop_front();
+  processingJobs_.push_back(job);
+  return job;
 }
 // print function
 ostream&TranslationJobRepository::print(ostream&os)const{
   lock_guard<mutex>lock(mtx_);
-  return os<<"#lanpairs: "<<allJobs_.size()<<endl;
+  return os<<"lanpair:: "<<lp_<<endl;
 }
 // debug print function
 ostream&operator<<(ostream&os,TranslationJobRepository const&r){
