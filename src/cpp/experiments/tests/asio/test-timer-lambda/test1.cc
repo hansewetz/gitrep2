c38e6776d9aeb0c96b3c959c5714d1247a4ac669
hans <hans@hans-HP-EliteBook-8740w.(none)>
2014-06-29 20:39:54 +0200

    ewetzha:L added lambda functuion completiuon callaback to asio

---
+++ src/cpp/experiments/tests/asio/test-timer-lambda/test1.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <functional>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+using namespace std;
+
+// contger and max count
+int cnt{0};
+int maxCnt{5};
+
+// io service
+boost::asio::io_service ios;
+
+// main.
+int main(int argc,char**argv){
+  // setup timer  (IO object)
+  boost::asio::deadline_timer tmo(::ios,boost::posix_time::seconds(2));
+
+  // completion handler for timer
+  std::function<void(boost::system::error_code)>f=[&](boost::system::error_code err){
+        // check iof we are onde
+        cerr<<"TMO"<<endl;
+        if(++cnt>maxCnt)return;
+
+        // trigger timer again
+        tmo.expires_from_now(boost::posix_time::seconds(2));
+        tmo.async_wait(f);
+      };
+
+  // start timer (specify completion handler) and kick off event loop
+  tmo.async_wait(f);
+  ::ios.run();
+}
+
