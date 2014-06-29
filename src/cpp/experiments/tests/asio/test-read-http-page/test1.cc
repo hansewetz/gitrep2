02d36d0fcb81fd5b53a09cc7a39c8b15788bd505
hans <hans@hans-HP-EliteBook-8740w.(none)>
2014-06-29 22:05:40 +0200

    ewetzha: added reading http page

---
+++ src/cpp/experiments/tests/asio/test-read-http-page/test1.cc
@@ -0,0 +1,24 @@
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio.hpp>
+#include <string>
+
+// main.
+int main(int argc,char**argv){
+  // setup socket stream
+  boost::asio::ip::tcp::iostream s("boost.org","http");
+  s.expires_from_now(boost::posix_time::seconds(2));
+
+  // send request
+  s<<"GET / HTTP/1.0\r0.2cm/n";
+  s<<"Host: www.boost.org\r\n";
+  s<<"Accept: /*\r\n";
+  s<<"Connection: close\r\n\r\n";
+
+  // get response
+  std::string header;
+  while(std::getline(s,header)&&header!="\r"){
+    std::cout<<header<<std::endl;
+  }
+  std::cout<<s.rdbuf();
+}
+
