6d7f664efbbd0c12bea9608a12aca2adac5f2f26
hans ewetz <hansewetz@hotmail.com>
2014-07-04 09:57:03 +0200

    ewetzha: added rebase to resources directory

---
+++ src/cpp/experiments/tests/resources/rebase/test1.cc
@@ -0,0 +1,52 @@
+#include "boost/iterator.hpp"
+#include <iostream>
+#include <vector>
+#include <memory>
+using namespace std;
+
+template<typename T,typename A=allocator<T>>
+class List{
+public:
+  struct Link;
+  using LinkAlloc=typename A:: template rebind<Link>::other;
+
+  // link in list
+  struct Link{
+    Link(int i):i_(i){}
+    int i_;
+    Link*next_=0;
+  };
+  void pushFront(int i){
+    // allocate memory and construct object in memory
+    Link*mem=linkAlloc_.allocate(sizeof(Link));
+    Link*lnk=new(mem)Link(i);
+
+     // insert into list
+    if(first==nullptr)first=lnk;
+    else{
+      lnk->next_=first;
+      first=lnk;
+    }
+  }
+  // print list
+  void print(ostream&os)const{
+    Link*next=first;
+    while(next){
+      cout<<next->i_<<endl;
+      next=next->next_;
+    }
+  }
+private:
+  // list state
+  Link*first=nullptr;
+
+  // allocators
+  LinkAlloc linkAlloc_;
+  A listAlloc_;
+};
+
+int main(){
+  List<int,std::allocator<int>>lst;
+  for(int i=0;i<10;++i)lst.pushFront(i);
+  lst.print(cout);
+}
