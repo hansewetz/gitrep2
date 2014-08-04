fe3e7bd8fd8ef75c97f19f90e8d189c9e234cd8a
hans ewetz <hansewetz@hotmail.com>
2014-08-04 15:04:33 +0200

    ewetzha: added bubble sort

---
+++ src/cpp/experiments/algorithms/sorting/bubble-sort/test1.cc
@@ -0,0 +1,24 @@
+#include <iterator>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// bubble sort
+// (scompare if (K(k+1) < K(k) swap them, repeat)
+void bsort(vector<int>&v){
+  size_t n{v.size()};
+  if(n<=1)return;
+  --n;
+  for(int i=n-1;i>=0;--i){
+    for(int j=0;j<=i;++j){
+      if(v[j+1]<v[j])swap(v[j+1],v[j]);
+    }
+  }
+}
+int main(){
+  vector<int>v{5,2,7,3,4,5,98,24};
+  bsort(v);
+  copy(v.begin(),v.end(),ostream_iterator<int>(cout," "));
+}
