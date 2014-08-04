aab7fea4e36fbf886814501b16329d5d3344ce3b
hans ewetz <hansewetz@hotmail.com>
2014-08-04 15:36:02 +0200

    ewetzha: added sort counting

---
+++ src/cpp/experiments/algorithms/sorting/count-sort/test1.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <random>
+#include <boost/timer/timer.hpp>
+
+using namespace std;
+
+// count sort
+// (count[i] ==> record i should be in place count[i])
+void csort(vector<int>&v){
+  int n=v.size();
+  vector<int>count(n);
+
+  // count #of items before other items
+  for(int i=0;i<n-1;++i){
+    for(int j=i+1;j<n;++j){
+      if(v[i]>v[j])++count[i];
+      else ++count[j];
+    }
+  }
+  // arrange items in sorted order
+  vector<int>res(n);
+  for(int i=0;i<n;++i){
+    res[count[i]]=v[i];
+  }
+  swap(res,v);
+}
+
+// test main program
+int main(){
+  vector<int>v{4,6,1,2,9,2,66,45,3,89};
+  csort(v);
+  copy(v.begin(),v.end(),ostream_iterator<int>(cout," "));
+}
