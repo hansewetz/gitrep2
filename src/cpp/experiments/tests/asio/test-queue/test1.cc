ce6fdc65fb4e13027607e9ac842d0378506f75e3
hans ewetz <hansewetz@hotmail.com>
2014-08-05 18:45:07 +0200

    ewetzha: minor changes in test program

--- src/cpp/experiments/tests/asio/test-queue/test1.cc
+++ src/cpp/experiments/tests/asio/test-queue/test1.cc
@@ -11,45 +11,45 @@ using namespace std;
 using namespace std::placeholders;
 
 // max #of messages to send
-constexpr size_t maxmsg1{10};
+constexpr size_t maxmsg{10};
 
 // underlying queue
 std::size_t maxqitems{3};
-shared_ptr<boost::asio::simple_queue<int>>q1{new boost::asio::simple_queue<int>(maxqitems)};
+shared_ptr<boost::asio::simple_queue<int>>q{new boost::asio::simple_queue<int>(maxqitems)};
 
 // asio io service
 boost::asio::io_service ios;
 
 // asio queue listener and sender
-boost::asio::simple_queue_listener<int>qlistener1(::ios);
-boost::asio::simple_queue_sender<int>qsender1(::ios);
+boost::asio::simple_queue_listener<int>qlistener(::ios);
+boost::asio::simple_queue_sender<int>qsender(::ios);
 
 // queue listener handler for queue 1
 size_t nreceived1{0};
 template<typename T>
-void qlistener_handler1(boost::system::error_code const&ec,T item,boost::asio::simple_queue_listener<T>*asioq,shared_ptr<boost::asio::simple_queue<int>>q1){
+void qlistener_handler(boost::system::error_code const&ec,T item,boost::asio::simple_queue_listener<T>*asioq,shared_ptr<boost::asio::simple_queue<int>>q){
   // print item if error code is OK
-  if(ec)BOOST_LOG_TRIVIAL(debug)<<"received item in qlistener_handler1 (via asio), item: <invalid>, ec: "<<ec;
+  if(ec)BOOST_LOG_TRIVIAL(debug)<<"received item in qlistener_handler (via asio), item: <invalid>, ec: "<<ec;
   else{
-    BOOST_LOG_TRIVIAL(debug)<<"received item in qlistener_handler1 (via asio), item: "<<item<<", ec: "<<ec;
-    if(++nreceived1!=maxmsg1)asioq->async_deq(q1,std::bind(qlistener_handler1<T>,_1,_2,asioq,q1));
+    BOOST_LOG_TRIVIAL(debug)<<"received item in qlistener_handler (via asio), item: "<<item<<", ec: "<<ec;
+    if(++nreceived1!=maxmsg)asioq->async_deq(q,std::bind(qlistener_handler<T>,_1,_2,asioq,q));
   }
 }
 // queue sender handler for queue 1
 size_t nsent1{0};
 template<typename T>
-void qsender_handler1(boost::system::error_code const&ec,boost::asio::simple_queue_sender<T>*asioq,shared_ptr<boost::asio::simple_queue<int>>q1,int item){
+void qsender_handler(boost::system::error_code const&ec,boost::asio::simple_queue_sender<T>*asioq,shared_ptr<boost::asio::simple_queue<int>>q,int item){
   // print item if error code is OK
-  if(ec)BOOST_LOG_TRIVIAL(debug)<<"sent item in qsender_handler1 (via asio), item: <invalid>, ec: "<<ec;
+  if(ec)BOOST_LOG_TRIVIAL(debug)<<"sent item in qsender_handler (via asio), item: <invalid>, ec: "<<ec;
   else{
-    BOOST_LOG_TRIVIAL(debug)<<"sending item in qlistener_handler1 (via asio), item: "<<item<<", ec: "<<ec;
-    if(++nsent1!=maxmsg1)asioq->async_enq(q1,item+1,std::bind(qsender_handler1<T>,_1,asioq,q1,item+1));
+    BOOST_LOG_TRIVIAL(debug)<<"sending item in qlistener_handler (via asio), item: "<<item<<", ec: "<<ec;
+    if(++nsent1!=maxmsg)asioq->async_enq(q,item+1,std::bind(qsender_handler<T>,_1,asioq,q,item+1));
   }
 }
 // timer handler
 void timer_handler(const boost::system::error_code&err){
   // start listening to messages
-  qlistener1.async_deq(q1,std::bind(qlistener_handler1<int>,_1,_2,&qlistener1,q1));
+  qlistener.async_deq(q,std::bind(qlistener_handler<int>,_1,_2,&qlistener,q));
 }
 // test program
 int main(){
@@ -59,7 +59,7 @@ int main(){
     tmo.async_wait(timer_handler);
 
     // start sending messages
-    qsender1.async_enq(q1,1,std::bind(qsender_handler1<int>,_1,&qsender1,q1,1));
+    qsender.async_enq(q,1,std::bind(qsender_handler<int>,_1,&qsender,q,1));
 
     // run io service and join sender thread
     ::ios.run();
