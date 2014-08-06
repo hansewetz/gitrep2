d926906b0780cd69abc55d1102e6d15862644a39
hans ewetz <hansewetz@hotmail.com>
2014-08-06 10:32:58 +0200

    ewetzha: updated test program

--- src/cpp/experiments/tests/asio/test-queue/test1.cc
+++ src/cpp/experiments/tests/asio/test-queue/test1.cc
@@ -17,48 +17,41 @@ constexpr size_t maxmsg{10};
 std::size_t maxqitems{3};
 shared_ptr<boost::asio::simple_queue<int>>q{new boost::asio::simple_queue<int>(maxqitems)};
 
-// asio objects
+// asio queue stuff
 boost::asio::io_service ios;
 boost::asio::simple_queue_listener<int>qlistener(::ios,q);
 boost::asio::simple_queue_sender<int>qsender(::ios,q);
-boost::asio::deadline_timer tmo(::ios,boost::posix_time::seconds(3));
 
-// queue listener handler for queue
-size_t nreceived{0};
+// handler for queue listener
 template<typename T>
-void qlistener_handler(boost::system::error_code const&ec,T item){
+void qlistener_handler(boost::system::error_code const&ec,T item, int nreceived){
   // print item if error code is OK
   if(ec)BOOST_LOG_TRIVIAL(debug)<<"queue listener interupted (via asio): ignoring callback queue item, ec: "<<ec;
   else{
     BOOST_LOG_TRIVIAL(debug)<<"received item in qlistener_handler (via asio), item: "<<item<<", ec: "<<ec;
-    if(++nreceived<maxmsg)qlistener.async_deq(std::bind(qlistener_handler<T>,_1,_2));
+    if(nreceived+1<maxmsg)qlistener.async_deq(std::bind(qlistener_handler<T>,_1,_2,nreceived+1));
   }
 }
-// queue sender handler for queue
-size_t nsent{0};
+// handler for queue sender
 template<typename T>
-void qsender_handler(boost::system::error_code const&ec,int item){
+void qsender_handler(boost::system::error_code const&ec,int item,int nsent){
   // print item if error code is OK
   if(ec)BOOST_LOG_TRIVIAL(debug)<<"queue sender interupted (via asio): ignoring callback, ec: "<<ec;
   else{
     BOOST_LOG_TRIVIAL(debug)<<"sending item in qlistener_handler (via asio), item: "<<item<<", ec: "<<ec;
-    if(++nsent<maxmsg)qsender.async_enq(item+1,std::bind(qsender_handler<T>,_1,item+1));
+    if(nsent+1<maxmsg)qsender.async_enq(item+1,std::bind(qsender_handler<T>,_1,item+1,nsent+1));
   }
-}
-// timer handler
-void timer_handler(const boost::system::error_code&err){
-  // start listening to messages
-  qlistener.async_deq(std::bind(qlistener_handler<int>,_1,_2));
 }
 // test program
 int main(){
   try{
-    // setup timer to trigger after which we start listening to messages
-    tmo.async_wait(timer_handler);
+    // setup timer to trigger after 3 seconds aftr which we start listening to messages
+    boost::asio::deadline_timer tmo(::ios,boost::posix_time::seconds(3));
+    tmo.async_wait([&](const boost::system::error_code&ec){qlistener.async_deq(std::bind(qlistener_handler<int>,_1,_2,0));});
 
     // start sending messages
     int startItem{1};
-    qsender.async_enq(startItem,std::bind(qsender_handler<int>,_1,startItem));
+    qsender.async_enq(startItem,std::bind(qsender_handler<int>,_1,startItem,0));
 
     // run io service and join sender thread
     ::ios.run();
