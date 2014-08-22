1ae2583ec20328ffc9c0dbba262ad39dd188ac8b
hans ewetz <hansewetz@hotmail.com>
2014-08-22 15:53:12 +0200

    ewetzha: started test program for boost process

---
+++ src/cpp/experiments/tests/test-boost-process/test1.cc
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <boost/process.hpp>
+using namespace std;
+
+// start a child
+boost::process::child startChild(){
+}
+// main test program
+int main(){
+}
