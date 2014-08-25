78fcf4d9e078d0088835ed0a29c8ca8a248c233c
hans ewetz <hansewetz@hotmail.com>
2014-08-25 13:41:43 +0200

    ewetzha: started to add test cases for boost process

--- src/cpp/experiments/tests/test-boost-process/test1.cc
+++ src/cpp/experiments/tests/test-boost-process/test1.cc
@@ -2,11 +2,14 @@
 #include <vector>
 #include <iostream>
 #include <boost/process.hpp>
+#include <boost/filesystem.hpp>
 using namespace std;
+namespace bp=boost::process;
+namespace fs=boost::filesystem;
 
-// start a child
-boost::process::child startChild(){
-}
 // main test program
 int main(){
+  fs::path prg{"/bin/ls"};
+  bp::child c{bp::execute(bp::initializers::run_exe(prg))};
+  bp::wait_for_exit(c);
 }
