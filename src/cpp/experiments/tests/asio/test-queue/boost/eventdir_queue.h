fbd13428fc79f0d2afdb123bea14a62b411f67b4
hans ewetz <hansewetz@hotmail.com>
2014-10-07 16:06:25 +0200

    ewetzha: rorg of boost asio queue + starting to code eventdir_queue

---
+++ src/cpp/experiments/tests/asio/test-queue/boost/eventdir_queue.h
@@ -0,0 +1,142 @@
+#ifndef __EVENTDIR_QUEUE_H__
+#define __EVENTDIR_QUEUE_H__
+#include <string>
+#include <utility>
+#include <list>
+#include <map>
+#include <fstream>
+#include <boost/filesystem.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/interprocess/sync/scoped_lock.hpp>
+#include <boost/interprocess/sync/named_mutex.hpp>
+#include <boost/interprocess/sync/named_condition.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+namespace boost{
+namespace asio{
+
+namespace fs=boost::filesystem;
+namespace ipc=boost::interprocess;
+namespace pt= boost::posix_time;
+
+template<typename T,typename DESER,typename SERIAL>
+class eventdir_queue{
+public:
+  // typedef for value stored in queue
+  // (need this so we can create an item with default ctor)
+  using value_type=T;
+
+  // typedef for serialiser/de-serialiser
+  using deserialiser=DESER;
+  using serialiser=SERIAL;
+
+  // ctors,assign,dtor
+  eventdir_queue(std::size_t maxsize,fs::path const&dir,DESER deser,SERIAL serial,bool removelocks):
+      maxsize_(maxsize),dir_(dir),deser_(deser),serial_(serial),removelocks_(removelocks),
+      ipcmtx_(ipc::open_or_create,getMutexName(dir).c_str()),ipccond_(ipc::open_or_create,getCondName(dir).c_str()){
+    // make sure path is a directory
+    if(!fs::is_directory(dir_))throw std::logic_error(std::string("eventdir_queue::eventdir_queue: dir_: ")+dir.string()+" is not a directory");
+
+    // setup to receive events from inotify
+    // ...
+
+    // lock directory and read content
+    // ...
+  }
+  eventdir_queue(eventdir_queue const&)=delete;
+  eventdir_queue(eventdir_queue&&)=default;
+  eventdir_queue&operator=(eventdir_queue const&)=delete;
+  eventdir_queue&operator=(eventdir_queue&&)=default;
+  ~eventdir_queue(){
+    if(removelocks_)removeLockVariables(dir_);
+  }
+  // put a message into queue
+  // (returns true if message was enqueued, false if enqueing was disabled)
+  bool enq(T t){
+    // NOTE! Not yet done
+  }
+  // wait until we can put a message in queue
+  // (returns false if enqueing was disabled, else true)
+  bool wait_enq(){
+    // NOTE! Not yet done
+  }
+  // dequeue a message (return.first == false if deq() was disabled)
+  std::pair<bool,T>deq(){
+    // NOTE! Not yet done
+  }
+  // cancel deq operations (will also release blocking threads)
+  void disable_deq(bool disable){
+    ipc::scoped_lock<ipc::named_mutex>lock(ipcmtx_);
+    deq_enabled_=!disable;
+    ipccond_.notify_all();
+  }
+  // cancel enq operations (will also release blocking threads)
+  void disable_enq(bool disable){
+    ipc::scoped_lock<ipc::named_mutex>lock(ipcmtx_);
+    enq_enabled_=!disable;
+    ipccond_.notify_all();
+  }
+  // set max size of queue
+  void set_maxsize(std::size_t maxsize){
+    ipc::scoped_lock<ipc::named_mutex>lock(ipcmtx_);
+    return maxsize_;
+  }
+  // check if queue is empty
+  bool empty()const{
+    // NOTE! Not yet done
+  }
+  // check if queue is full
+  bool full()const{
+    // NOTE! Not yet done
+  }
+  // get #of items in queue
+  std::size_t size()const{
+    // NOTE! Not yet done
+  }
+  // get max items in queue
+  std::size_t maxsize()const{
+    ipc::scoped_lock<ipc::named_mutex>lock(ipcmtx_);
+    return maxsize_;
+  }
+  // remove lock variables for queue
+  static void removeLockVariables(fs::path const&dir){
+    boost::interprocess::named_mutex::remove(getMutexName(dir).c_str());
+    boost::interprocess::named_condition::remove(getCondName(dir).c_str());
+  }
+private:
+  // create a name for mutex for this dircetory queue queue
+  static std::string getMutexName(fs::path const&dir){
+    std::string sdir{dir.string()};
+    std::replace_if(sdir.begin(),sdir.end(),[](char c){return c=='/'||c=='.';},'_');
+    return sdir;
+  }
+  // create a name for condition variable for this directory queue
+  static std::string getCondName(fs::path const&dir){
+    std::string sdir{dir.string()};
+    std::replace_if(sdir.begin(),sdir.end(),[](char c){return c=='/'||c=='.';},'_');
+    return sdir;
+  }
+  // user specified characteristics of queue
+  std::size_t maxsize_;
+  fs::path dir_;
+  bool removelocks_;
+
+  // serialization/deserialization functions
+  DESER deser_;
+  SERIAL serial_;
+
+  // state of queue
+  bool deq_enabled_=true;
+  bool enq_enabled_=true;
+
+  // mutex/condition variables
+  mutable boost::interprocess::named_mutex ipcmtx_;
+  mutable boost::interprocess::named_condition ipccond_;
+
+  // cache of message
+  mutable std::list<fs::path>cache_;
+};
+}
+}
+#endif
---
+++ src/cpp/experiments/tests/asio/test-queue/tests/test-7/test7.cc
@@ -0,0 +1,59 @@
+/*
+  test program for eventdir_queue.
+  program kicks off a sender thread and a receiver thread and terminates when a specified #of messages has been received
+*/
+#include <boost/asio_queue.h>
+#include <string>
+#include <thread>
+#include <functional>
+
+using namespace std;
+namespace asio= boost::asio;
+namespace fs=boost::filesystem;
+
+// sender
+template<typename Q>
+void sender(Q&q,size_t maxmsg){
+  for(int i=0;i<maxmsg;++i){
+    q.enq(i);
+    std::chrono::milliseconds tmo(1000);
+    std::this_thread::sleep_for(tmo);
+  }
+}
+// receiver
+template<typename Q>
+void receiver(Q&q,size_t maxmsg){
+  while(maxmsg!=0){
+    pair<bool,int>p{q.deq()};
+    cout<<"deq: "<<boolalpha<<"["<<p.first<<","<<p.second<<"]"<<endl;
+    --maxmsg;
+  }
+}
+// test main program
+int main(int argc,char*argv[]){
+  try{
+    // directory for queue
+    fs::path qdir{"./q1"};
+
+    // setup queue
+    function<int(istream&)>reader=[](istream&is){int ret;is>>ret;return ret;};
+    function<void(ostream&,int)>writer=[](ostream&os,int i){os<<i;};
+    asio::eventdir_queue<int,decltype(reader),decltype(writer)>q1{10,qdir,reader,writer,true};
+
+    // remove locks if they exist
+    q1.removeLockVariables(qdir);
+
+    // kick off threads for sender/receiver
+    size_t maxmsg{10};
+    std::thread tsender{[&](){sender(q1,maxmsg);}};
+    std::thread treceiver{[&](){receiver(q1,maxmsg);}};
+
+    // join threads
+    treceiver.join();
+    tsender.join();
+  }
+  catch(exception const&e){
+    cerr<<"caught exception: "<<e.what()<<endl;
+    exit(1);
+  }
+}
