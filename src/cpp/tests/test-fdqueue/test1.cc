f84a1cff95b4b561afc2e1bbf6837bb2cd8ddee2
hans ewetz <hansewetz@hotmail.com>
2014-11-19 13:53:36 +0100

    ewetzha: updates to fdqueue test program

--- src/cpp/tests/test-fdqueue/test1.cc
+++ src/cpp/tests/test-fdqueue/test1.cc
@@ -1,6 +1,6 @@
 /*
 This program:
-	- setup of queue_sender<> and queue_listener<> connected to child process
+	- start child process and setup of queue_sender<> and queue_listener<> connected to child process stdin/stdout
 		- creates a 2 pipes and spawns a child process connecting the two
 		- stdin/stdout in the child are duped to the pipe ends on the child side
 		- an asio::fdenq_queue is created from the write fd on the parent side
@@ -13,11 +13,11 @@ This program:
 */
 #include "asio-extensions/asio_queue.hpp"
 #include "utils/sysUtils.h"
-#include "utils/logUtils.h"
+#include <vector>
+#include <functional>
 #include <iostream>
-#include <unistd.h>
+#include <string>
 #include <sys/wait.h>
-#include <boost/asio.hpp>
 #include <boost/log/trivial.hpp>
 
 using namespace std;
@@ -27,46 +27,49 @@ namespace asio=boost::asio;
 
 // ----- some constants -----
 namespace {
-size_t msgcount{0};
-constexpr size_t maxmsg{20};
-constexpr size_t tmo_deq_ms{2000};
-constexpr size_t tmo_enq_ms{1000};
+size_t msgcount{0};                 // track #of messages sent
+constexpr size_t maxmsg{5};        // we'll send this many messages
+constexpr size_t tmo_deq_ms{2000};  // timeout for waiting async. for a message
+constexpr size_t tmo_enq_ms{1000};  // timeout waiting to be able to sen a message async.
 }
-// io_service object
- boost::asio::io_service ios;
+// io_service object (we are asio based)
+boost::asio::io_service ios;
 
-// serialize an object (notice: message boundaries are on '\n' characters)
+// serialize an object to be sent (notice: message boundaries are on '\n' characters)
+// (normally messages would be serialized and then base64 encoded)
 std::function<void(std::ostream&,string const&)>serialiser=[](std::ostream&os,string const&s){
   os<<s;
 };
-// de-serialize an object (notice: message boundaries are on '\n' characters)
+// de-serialize an object we received (notice: message boundaries are on '\n' characters)
+// (normally messages would be base64-decoded and then deserialised)
 std::function<string(istream&)>deserialiser=[](istream&is){
   string line;
   getline(is,line);
   return line;
 };
-// queue types
+// queue types used by parent
+// (these are queues used by the parent - the child reads/writes directly to stdin/stdout)
 using enq_t=asio::fdenq_queue<string,decltype(serialiser)>;
 using deq_t=asio::fddeq_queue<string,decltype(deserialiser)>;
 
-// handler for queue listener
+// handle a message we received asynchrounosly
 void qlistener_handler(boost::system::error_code const&ec,string msg,asio::queue_listener<deq_t>*ql){
-  if(ec!=0){
-    BOOST_LOG_TRIVIAL(info)<<"deque() aborted (via asio), ec: "<<ec.message();
+  if(ec!=0)BOOST_LOG_TRIVIAL(info)<<"deque() aborted (via asio), cause: "<<ec.message();
   }else{
-    BOOST_LOG_TRIVIAL(info)<<"received msg in qlistener_handler (via asio), msg: \""<<msg<<"\", ec: "<<ec;
+    // kick off waiting for another message asynchrounosly
+    BOOST_LOG_TRIVIAL(info)<<"received msg in qlistener_handler (via asio), msg: \""<<msg<<"\", cause: "<<ec;
     ql->timed_async_deq(std::bind(qlistener_handler,_1,_2,ql),tmo_deq_ms);
   }
 }
-// handler for queue sender
+// handler called asynchrounosly when 'it is possible' to send a message through the pipe
 void qsender_handler(boost::system::error_code const&ec,asio::queue_sender<enq_t>*qs){
   // print item if error code is OK
-  if(ec)BOOST_LOG_TRIVIAL(info)<<"queue sender interupted (via asio): ignoring callback, ec: "<<ec;
+  if(ec)BOOST_LOG_TRIVIAL(info)<<"queue sender interupted (via asio): ignoring callback, cause: "<<ec;
   else{
     // check if we are done
     if(msgcount==maxmsg)return;
 
-    // sent next message asynchrounously
+    // kick off waiting to be able to send another message asynchrounosly
     string msg{boost::lexical_cast<string>(msgcount++)};
     qs->timed_async_enq(msg,std::bind(qsender_handler,_1,qs),tmo_enq_ms);
   }
@@ -74,15 +77,14 @@ void qsender_handler(boost::system::error_code const&ec,asio::queue_sender<enq_t
 // main for test program
 int main(){
   try{
-    // set log level (true: log debug info, false: do not log debug info)
-    utils::initBoostFileLogging(false);
-
     // spawn '/bin/cat' as child process
     int fdread,fdwrite;
     int cpid=spawnPipeChild("/bin/cat",vector<string>{"cat"},fdread,fdwrite,true);
 
-    // create queues
+    // error code from asio calls
     boost::system::error_code ec;
+
+    // create queues
     enq_t qsend{fdwrite,serialiser};
     deq_t qrecv{fdread,deserialiser};
     
@@ -90,22 +92,23 @@ int main(){
     asio::queue_sender<enq_t>qsender(::ios,&qsend);
     asio::queue_listener<deq_t>qlistener(::ios,&qrecv);
 
-    // listen for messages asynchronously
+    // kick off listening for messages asynchronously
     BOOST_LOG_TRIVIAL(debug)<<"start sending messages asynchrounously ...";
     string msg{boost::lexical_cast<string>(msgcount++)};
     qsender.timed_async_enq(msg,std::bind(qsender_handler,_1,&qsender),tmo_enq_ms);
 
-    // send messages asynchronously
+    // kick off sending messages asynchronously
     BOOST_LOG_TRIVIAL(info)<<"starting waiting for asio message ...";
     qlistener.timed_async_deq(std::bind(qlistener_handler,_1,_2,&qlistener),tmo_deq_ms);
 
+    // run asio
     BOOST_LOG_TRIVIAL(info)<<"starting asio ...";
     ::ios.run();
 
     // cleanup
     BOOST_LOG_TRIVIAL(info)<<"closing fds ...";
-    close(fdwrite);
-    close(fdread);
+    eclose(fdwrite);
+    eclose(fdread);
 
     // wait for child
     BOOST_LOG_TRIVIAL(info)<<"waiting for child (pid: "<<cpid<<") ..."<<endl;
