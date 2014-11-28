4c5dc129ae46e4c31472a9bf03341d07613a341b
hans ewetz <hansewetz@hotmail.com>
2014-11-28 13:21:50 +0100

    ewetzha: added function for sorting tasks by segment number

--- src/cpp/libs/xlate-tools/src/JobHandlerScreenPrinter.cc
+++ src/cpp/libs/xlate-tools/src/JobHandlerScreenPrinter.cc
@@ -1,6 +1,7 @@
 // (C) Copyright Hans Ewetz 2010,2011,2012,2013,2014. All rights reserved.
 
 #include "xlate-tools/JobHandlerScreenPrinter.h"
+#include "xlate-tools/utils.h"
 #include "xlate-jobs/TranslationTask.h"
 #include "xlate-jobs/TranslationJob.h"
 #include <iostream>
@@ -13,10 +14,12 @@ JobHandlerScreenPrinter::JobHandlerScreenPrinter(shared_ptr<TranslationJob>job):
 }
 // virtual function call - print job to screen
 void JobHandlerScreenPrinter::operator()(){
+  // sort tasks by segno
+  vector<shared_ptr<TranslationTask>>tasks{getTaskListSortedBySegNo({job_->translated()})};
+
   // print translated segments
   cout<<endl<<"------------------------------------- job#: "<<job_->id()<<" -------------------------------------"<<endl;
-  list<shared_ptr<TranslationTask>>const&translated{job_->translated()};
-  for(shared_ptr<TranslationTask>task:translated)cout<<"["<<task->srcSeg()<<"]-->["<<task->targetSeg()<<"][segno: "<<task->segno()<<"][id: "<<task->id()<<"][engine: "<<task->engineId()<<"]"<<endl;
+  for(shared_ptr<TranslationTask>task:tasks)cout<<"["<<task->srcSeg()<<"]-->["<<task->targetSeg()<<"][segno: "<<task->segno()<<"][id: "<<task->id()<<"][engine: "<<task->engineId()<<"]"<<endl;
   cout<<"------------------------------------------------------------------------------"<<endl;
 }
 // print debug info about handler
