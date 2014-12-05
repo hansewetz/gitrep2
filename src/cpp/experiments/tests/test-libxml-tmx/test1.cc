9dc7ad5252a6e3b6f12d0bba905c377f07aae883
hans ewetz <hansewetz@hotmail.com>
2014-12-05 15:03:55 +0100

    ewetzha: work on tmx parsing

--- src/cpp/experiments/tests/test-libxml-tmx/test1.cc
+++ src/cpp/experiments/tests/test-libxml-tmx/test1.cc
@@ -1,38 +1,69 @@
 // (C) Copyright Hans Ewetz 2010,2011,2012,2013,2014. All rights reserved.
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
 #include <stdio.h>
 #include <string.h>
 #include <libxml/xmlreader.h>
 using namespace std;
 
+// tags to look for.
+const xmlChar*header_tag=(unsigned char*)"header";
+const xmlChar*prop_tag=(unsigned char*)"prop";
+const xmlChar*tuv_tag=(unsigned char*)"tuv";
+const xmlChar*tu_tag=(unsigned char*)"tu";
+const xmlChar*seg_tag=(unsigned char*)"seg";
+const xmlChar*text_tag=(unsigned char*)"#text";
 
-// states used to track where we should be
+// various property text strings
+const xmlChar*dbid_attr_value=(unsigned char*)"x-TMName";
+
+// valid languages
+static vector<const xmlChar*>lans{(unsigned char*)"nb-NO",(unsigned char*)"nn-NO",(unsigned char*)"en-GB"};
+
+// state related variables
 enum states{NOT_STARTED=0,HEADER=1,TU=2,TUV=3,SEG=4};
 static states state=NOT_STARTED;
+static bool getdbidFromHeader{false};
+static string currLan;
 
-// some other state related variables
-static string currlan;
-
+// check if tag contains a specific attribute
+bool tagHasDbid(xmlNodePtr node){
+  for(xmlAttrPtr attr=node->properties;NULL!=attr;attr=attr->next){
+    xmlChar*attrname=xmlNodeListGetString(node->doc,attr->children,1);
+    if(!xmlStrcmp(dbid_attr_value,attrname)){
+      xmlFree(attrname);
+      return true;
+    }
+  }
+  return false;
+}
+// get language from tuv attribute
+string getLanAttr(xmlNodePtr node){
+  for(xmlAttrPtr attr=node->properties;NULL!=attr;attr=attr->next){
+    xmlChar*attrname=xmlNodeListGetString(node->doc,attr->children,1);
+    for(int i=0;i<lans.size();++i){
+      if(!xmlStrcmp(lans[i],attrname)){
+        string ret{string((char*)lans[i])};
+        xmlFree(attrname);
+        return ret;
+      }
+    }
+    xmlFree(attrname);
+  }
+  // we did not find the language
+  throw runtime_error("missing language code in tuv");
+}
 // We assume the file is correctly formatted so we can rely on our state
 // (if we receive a tag we should not receive in the current state, then we toss an exception)
 static void processNode(xmlTextReaderPtr reader){
   const xmlChar*name,*value;
   bool opening;
 
-  // tags to look for.
-  const xmlChar*header_tag=(unsigned char*)"header";
-  const xmlChar*prop_tag=(unsigned char*)"prop";
-  const xmlChar*tuv_tag=(unsigned char*)"tuv";
-  const xmlChar*tu_tag=(unsigned char*)"tu";
-  const xmlChar*seg_tag=(unsigned char*)"seg";
-  const xmlChar*text_tag=(unsigned char*)"#text";
-
-  // various property text strings
-  const xmlChar*dbid_attr=(unsigned char*)"x-TMName";
-
   // get name of target together with value, and open/close tag
-  name = xmlTextReaderConstName(reader);
+  name=xmlTextReaderConstName(reader);
   value=xmlTextReaderConstValue(reader);
   opening=xmlTextReaderNodeType(reader)==1;
 
@@ -50,22 +81,19 @@ static void processNode(xmlTextReaderPtr reader){
       if(opening&&!xmlStrcmp(name,tu_tag)){
         state=TU;
       }else
-      if(opening&&!xmlStrcmp(name,prop_tag)){
-        for(xmlAttrPtr attr=node->properties;NULL!=attr;attr=attr->next){
-          xmlChar*attrname=xmlNodeListGetString(node->doc,attr->children,1);
-          // check if we have property for DBID
-          if(!xmlStrcmp(dbid_attr,attrname)){
-            printf("%s --> %s\n",attrname,value);
-            xmlFree(attrname);
-            break;
-          }
-        }
+      if(opening&&!xmlStrcmp(name,prop_tag)&&tagHasDbid(node)){
+        getdbidFromHeader=true;
+      }else
+      if(getdbidFromHeader&&!xmlStrcmp(name,text_tag)){
+        getdbidFromHeader=false;
+        printf("DBID: %s\n",value);
       }
       break;
     case TU:
       if(opening&&!xmlStrcmp(name,tuv_tag)){
+        currLan=getLanAttr(node);
         state=TUV;
-     }
+       }
      break;
     case TUV:
       if(opening&&!xmlStrcmp(name,seg_tag)){
@@ -73,7 +101,11 @@ static void processNode(xmlTextReaderPtr reader){
       }else
       if(!opening&&!xmlStrcmp(name,tu_tag)){
         state=TU;
-      }
+      }else
+      if(opening&&!xmlStrcmp(name,tuv_tag)){
+        currLan=getLanAttr(node);
+        state=TUV;
+       }
       break;
     case SEG:
       if(!opening&&!xmlStrcmp(name,tu_tag)){
@@ -83,11 +115,12 @@ static void processNode(xmlTextReaderPtr reader){
         state=TUV;
       }else
       if(!xmlStrcmp(name,text_tag)){
-// NOTE!        printf("%s",value);
+        printf("LAN: %s, SEG: %s",currLan.c_str(),value);
       }
       break;
   }
 }
+
 // process file
 static void streamFile(const char*filename) {
   xmlTextReaderPtr reader;
@@ -117,17 +150,3 @@ int main(int argc, char **argv) {
   streamFile(argv[1]);
   return(0);
 }
-
-/*
-
-  // NOTE! property
-  xmlNodePtr node=xmlTextReaderCurrentNode(reader);
-  for(xmlAttrPtr attr=node->properties;NULL!=attr;attr=attr->next){
-    xmlChar*v=xmlNodeListGetString(node->doc,attr->children,1);
-    printf("%s\n",v);
-    xmlFree(v);
-  }
-return;
-*/
-
-
