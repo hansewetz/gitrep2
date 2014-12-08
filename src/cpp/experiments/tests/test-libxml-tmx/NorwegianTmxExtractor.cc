ce284bdb0dceb08661730839e56ecda8280b5756
hans ewetz <hansewetz@hotmail.com>
2014-12-08 14:23:38 +0100

    ewetzha: update of tmx parsing of Norwegian tmx parsing

---
+++ src/cpp/experiments/tests/test-libxml-tmx/NorwegianTmxExtractor.cc
@@ -0,0 +1,183 @@
+// (C) Copyright Hans Ewetz 2010,2011,2012,2013,2014. All rights reserved.
+
+#include "NorwegianTmxExtractor.h"
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <stdexcept>
+#include <stdio.h>
+#include <string.h>
+#include <libxml/xmlreader.h>
+#include <boost/algorithm/string.hpp>
+using namespace std;
+namespace mtdata{
+
+// helper class for holding segments
+string const&NorwegianTmxExtractor::Segment::srclan()const{return srclan_;}
+string const&NorwegianTmxExtractor::Segment::targetlan()const{return targetlan_;}
+string const&NorwegianTmxExtractor::Segment::srcseg()const{return srcseg_;}
+string const&NorwegianTmxExtractor::Segment::targetseg()const{return targetseg_;}
+
+// print debug operator for Segment
+ostream&operator<<(ostream&os,NorwegianTmxExtractor::Segment const&s){
+  return os<<"["<<s.srclan()<<"]["<<s.targetlan()<<"]["<<s.srcseg()<<"]["<<s.targetseg()<<"]";
+}
+
+// static data and functions
+namespace{
+  // tags to look for.
+  const xmlChar*header_tag=(unsigned char*)"header";
+  const xmlChar*prop_tag=(unsigned char*)"prop";
+  const xmlChar*tuv_tag=(unsigned char*)"tuv";
+  const xmlChar*tu_tag=(unsigned char*)"tu";
+  const xmlChar*seg_tag=(unsigned char*)"seg";
+  const xmlChar*text_tag=(unsigned char*)"#text";
+
+  // various property text strings
+  const xmlChar*dbid_attr_value=(unsigned char*)"x-TMName";
+
+  // valid languages
+  vector<const xmlChar*>lans{(unsigned char*)"nb-NO",(unsigned char*)"nn-NO",(unsigned char*)"en-GB"};
+
+  // helper function: check if tag contains a specific attribute
+  bool tagHasDbid(xmlNodePtr node){
+    for(xmlAttrPtr attr=node->properties;NULL!=attr;attr=attr->next){
+      xmlChar*attrname=xmlNodeListGetString(node->doc,attr->children,1);
+      if(!xmlStrcmp(dbid_attr_value,attrname)){
+        xmlFree(attrname);
+        return true;
+      }
+    }
+    return false;
+  }
+  // helper function: get language from tuv attribute
+  string getLanAttr(xmlNodePtr node){
+    for(xmlAttrPtr attr=node->properties;NULL!=attr;attr=attr->next){
+      xmlChar*attrname=xmlNodeListGetString(node->doc,attr->children,1);
+      for(int i=0;i<lans.size();++i){
+        if(!xmlStrcmp(lans[i],attrname)){
+          string ret{string((char*)lans[i])};
+          ret=ret.substr(0,2);
+          xmlFree(attrname);
+          return ret;
+        }
+      }
+      xmlFree(attrname);
+    }
+    // we did not find the language
+    throw runtime_error("missing language code in tuv");
+  }
+}
+// ctor
+NorwegianTmxExtractor::NorwegianTmxExtractor(boost::filesystem::path const&tmxpath){
+  // check if we could open file for xl parsing using libxml
+  xmlTextReaderPtr reader{(xmlReaderForFile(tmxpath.string().c_str(),NULL,0))};
+  if(reader==NULL){
+    throw runtime_error(string("could not open file: ")+tmxpath.string()+" for libxml parsing");
+  }
+  // scan entire file - toss exception if error
+  int ret=xmlTextReaderRead(reader);
+  while (ret==1) {
+    processNode(reader);
+    ret=xmlTextReaderRead(reader);
+  }
+  xmlFreeTextReader(reader);
+  if (ret!=0){
+    runtime_error(string("failed to parse tmx file: ")+tmxpath.string()+" during libxml parsing");
+  }
+}
+// getters
+string NorwegianTmxExtractor::dbid()const{
+  return dbid_;
+}
+vector<NorwegianTmxExtractor::Segment>NorwegianTmxExtractor::segs()const{
+  return segs_;
+}
+
+
+// process node (called repreatadly)
+void NorwegianTmxExtractor::processNode(xmlTextReaderPtr reader){
+  const xmlChar*name,*value;
+  bool opening;
+
+  // get name of target together with value, and open/close tag
+  name=xmlTextReaderConstName(reader);
+  value=xmlTextReaderConstValue(reader);
+  opening=xmlTextReaderNodeType(reader)==1;
+
+  // hget current node
+  xmlNodePtr node=xmlTextReaderCurrentNode(reader);
+
+  // process state transitions and get segments
+  switch(state_){
+    case NOT_STARTED:
+      if(opening&&!xmlStrcmp(name,header_tag)){
+        state_=HEADER;
+      }
+      break;
+    case HEADER:
+      if(opening&&!xmlStrcmp(name,tu_tag)){
+        state_=TU;
+      }else
+      if(opening&&!xmlStrcmp(name,prop_tag)&&tagHasDbid(node)){
+        getdbidFromHeader_=true;
+      }else
+      if(getdbidFromHeader_&&!xmlStrcmp(name,text_tag)){
+        getdbidFromHeader_=false;
+        dbid_=string{(char*)value};
+      }
+      break;
+    case TU:
+      if(opening&&!xmlStrcmp(name,tuv_tag)){
+        currlan_=getLanAttr(node);
+        state_=TUV;
+        seg_=Segment{};
+       }
+     break;
+    case TUV:
+      if(opening&&!xmlStrcmp(name,seg_tag)){
+        state_=SEG;
+      }else
+      if(!opening&&!xmlStrcmp(name,tu_tag)){
+        // save segment
+        segs_.push_back(seg_);
+        seg_=Segment();
+        state_=TU;
+      }else
+      if(opening&&!xmlStrcmp(name,tuv_tag)){
+        currlan_=getLanAttr(node);
+        state_=TUV;
+       }
+      break;
+    case SEG:
+      if(!opening&&!xmlStrcmp(name,tu_tag)){
+        state_=TU;
+      }else
+      if(!opening&&!xmlStrcmp(name,tuv_tag)){
+        state_=TUV;
+      }else
+      if(!opening&&!xmlStrcmp(name,seg_tag)){
+        boost::algorithm::trim(currseg_);
+        boost::algorithm::trim_right(currseg_);
+
+        // save language and segment in segment we are building
+        if(currlan_=="en"){
+          seg_.srclan_=currlan_;
+          seg_.srcseg_=currseg_;
+        }else{
+          seg_.targetlan_=currlan_;
+          seg_.targetseg_=currseg_;
+        }
+        // we are done with one segment
+        currlan_="";
+        currseg_="";
+      }else
+      if(!xmlStrcmp(name,text_tag)){
+        // we might get tags inside the segment - append text as long as we are inside the seg tag
+        currseg_+=string((char*)value);
+      }
+      break;
+  }
+}
+}
---
+++ src/cpp/experiments/tests/test-libxml-tmx/NorwegianTmxExtractor.h
@@ -0,0 +1,71 @@
+#ifndef __NORWEGIAN_TMX_EXTRACTOR_H__
+#define __NORWEGIAN_TMX_EXTRACTOR_H__
+#include <string>
+#include <vector>
+#include <utility>
+#include <iosfwd>
+#include <boost/filesystem.hpp>
+#include <libxml/xmlreader.h>
+namespace mtdata{
+
+// class for parsing a Norwegian TMX file
+// (note: this class works with the tmx files provided by the Norwegian Ministry of Foreign Affairs)
+// (this class should not be used with any other data)
+class NorwegianTmxExtractor{
+public:
+  class Segment{
+  friend class NorwegianTmxExtractor;
+  public:
+    Segment(Segment const&)=default;
+    Segment(Segment&&)=default;
+    Segment&operator=(Segment const&)=default;
+    Segment&operator=(Segment&&)=default;
+    std::string const&srclan()const;
+    std::string const&targetlan()const;
+    std::string const&srcseg()const;
+    std::string const&targetseg()const;
+  private:
+    Segment()=default;
+    std::string srclan_;
+    std::string targetlan_;
+    std::string srcseg_;
+    std::string targetseg_;
+  };
+  // print debug operator for Segment
+  friend std::ostream&operator<<(std::ostream&os,Segment const&s);
+
+  // state related variables
+  enum states{NOT_STARTED=0,HEADER=1,TU=2,TUV=3,SEG=4};
+
+  // ctors,assign,dtor
+  // (make it non-movable an dnon copyable - no need to introduce complications)
+  NorwegianTmxExtractor(boost::filesystem::path const&tmxpath);
+  NorwegianTmxExtractor(NorwegianTmxExtractor const&)=delete;
+  NorwegianTmxExtractor(NorwegianTmxExtractor&&)=delete;
+  NorwegianTmxExtractor&operator=(NorwegianTmxExtractor const&)=delete;
+  NorwegianTmxExtractor&operator=(NorwegianTmxExtractor&&)=delete;
+  ~NorwegianTmxExtractor()=default;
+
+  // getters
+  std::string dbid()const;
+  std::vector<NorwegianTmxExtractor::Segment>segs()const;
+private:
+  // helper function
+  void processNode(xmlTextReaderPtr reader);
+
+  // state related variables
+  states state_=NorwegianTmxExtractor::NOT_STARTED;
+  boost::filesystem::path tmxpath_;
+
+  // some state variables used during extraction of data from tmx file
+  bool getdbidFromHeader_{false};
+  std::string currlan_{""};
+  std::string currseg_{""};
+  Segment seg_;
+
+  // data extracted from tmx file
+  std::string dbid_{""};
+  std::vector<NorwegianTmxExtractor::Segment>segs_;
+};
+}
+#endif
