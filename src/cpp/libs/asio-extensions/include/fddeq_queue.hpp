d93f9681f61e724ac5b6d22d42ac5d0c35293866
hans ewetz <hansewetz@hotmail.com>
2014-12-11 16:32:21 +0100

    ewetzha: updated asio-extension under lib with re-factored fd queues

--- src/cpp/libs/asio-extensions/include/fddeq_queue.hpp
+++ src/cpp/libs/asio-extensions/include/fddeq_queue.hpp
@@ -15,11 +15,14 @@ TESTING:
 
 #ifndef __FDDEQ_QUEUE_H__
 #define __FDDEQ_QUEUE_H__
+#include "detail/queue_support.hpp"
+#include "detail/fdqueue_support.hpp"
 #include <utility>
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <unistd.h>
+#include <boost/asio/error.hpp>
 
 namespace boost{
 namespace asio{
@@ -56,25 +59,25 @@ public:
   
   // dequeue a message (return.first == false if deq() was disabled)
   std::pair<bool,T>deq(boost::system::error_code&ec){
-    T ret{recvwait(0,ec,true)};
+    T ret{detail::queue_support::recvwait<T,DESER>(fdread_,0,ec,true,sep_,deser_)};
     if(ec!=boost::system::error_code())return std::make_pair(false,ret);
     return make_pair(true,ret);
   }
   // dequeue a message (return.first == false if deq() was disabled) - timeout if waiting too long
   std::pair<bool,T>timed_deq(std::size_t ms,boost::system::error_code&ec){
-    T ret{recvwait(ms,ec,true)};
+    T ret{detail::queue_support::recvwait<T,DESER>(fdread_,ms,ec,true,sep_,deser_)};
     if(ec!=boost::system::error_code())return std::make_pair(false,ret);
     return make_pair(true,ret);
   }
   // wait until we can retrieve a message from queue
   bool wait_deq(boost::system::error_code&ec){
-    recvwait(0,ec,false);
+    detail::queue_support::recvwait<T,DESER>(fdread_,0,ec,false,sep_,deser_);
     if(ec.value()!=0)return false;
     return true;
   }
   // wait until we can retrieve a message from queue -  timeout if waiting too long
   bool timed_wait_deq(std::size_t ms,boost::system::error_code&ec){
-    recvwait(ms,ec,false);
+    detail::queue_support::recvwait<T,DESER>(fdread_,ms,ec,false,sep_,deser_);
     if(ec==boost::asio::error::timed_out)return false;
     if(ec.value()!=0)return false;
     return true;
@@ -84,72 +87,6 @@ public:
     return fdread_;
   }
 private:
-  // deserialise an object from an fd stream
-  // or wait until there is a message to read - in this case, a default cibstructed object is returned
-  T recvwait(std::size_t ms,boost::system::error_code&ec,bool getMsg){
-    T ret;                            // return value from this function (default ctor if no error)
-    std::stringstream strstrm;        // collect read chars in a stringstream
-
-    // loop until we have a message (or until we timeout)
-    while(true){
-      // setup to listen on fd descriptor
-      fd_set input;
-      FD_ZERO(&input);
-      FD_SET(fdread_,&input);
-      int maxfd=fdread_;
-
-      // setup for timeout (ones we get a message we don't timeout)
-      struct timeval tmo;
-      tmo.tv_sec=ms/1000;
-      tmo.tv_usec=(ms%1000)*1000;
-      
-      // block on select - timeout if configured
-      assert(maxfd!=-1);
-      int n=::select(++maxfd,&input,NULL,NULL,ms>0?&tmo:NULL);
-
-      // check for error
-      if(n<0){
-        ec=boost::system::error_code(errno,boost::system::get_posix_category());
-        return T{};
-      }
-      // check for tmo
-      if(n==0){
-        ec=boost::asio::error::timed_out;
-        return T{};
-      }
-      // check if we got some data
-      if(FD_ISSET(fdread_,&input)){
-        // if we are only checking if we have a message we are done here
-        if(!getMsg){
-          ec= boost::system::error_code{};
-          return T{};
-        }
-        // read up to '\n' inclusive
-        ssize_t count{0};
-        while(true){
-          // read next character in message
-          char c;
-          ssize_t stat;
-          while((stat=::read(fdread_,&c,1))==EINTR){}
-          if(stat!=1){
-            // create a boost::system::error_code from errno
-            ec=boost::system::error_code(errno,boost::system::get_posix_category());
-            return T{};
-          }
-          // save character just read
-          strstrm<<c;
-
-          // if we reached newline, send message including newline)
-          if(c==sep_)return deser_(strstrm);
-        }
-        // check if we read all available characters
-        // (if there are no more chars to read, then restart select() statement)
-        if(++count==n)break;
-      }
-      // restet tmo 0 zero ms since we don't timeout ones we start reading a message
-      ms=0;
-    }
-  }
   // state of queue
   int fdread_;                           // file descriptors to read from from
   DESER deser_;                          // de-serialiser
