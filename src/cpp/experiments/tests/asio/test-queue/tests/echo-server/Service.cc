1bf7c4855ab3a83ebf0f0422a14300e12d532dfb
hans ewetz <hansewetz@hotmail.com>
2015-05-08 13:53:01 +0200

    ewetzha: added new asio queue test

---
+++ src/cpp/experiments/tests/asio/test-queue/tests/echo-server/Service.cc
@@ -0,0 +1,22 @@
+#include "Service.h"
+#include <functional>
+using namespace std;
+using namespace std::placeholders;
+namespace asio=boost::asio;
+
+// ctor
+Service::Service(asio::io_service&ios,shared_ptr<QueueDeqBase>qserviceReceive,shared_ptr<QueueEnqBase>qserviceReply):
+    serviceReceiver_(make_shared<QueueListener>(ios,qserviceReceive.get())),
+    serviceSender_(make_shared<QueueSender>(ios,qserviceReply.get())){
+  // setup asio callback
+  serviceReceiver_->async_deq(std::bind(&Service::recvHandler,this,_1,_2));
+}
+// handler for receiving message
+void Service::recvHandler(boost::system::error_code const&ec,QueueValueType const&msg){
+  // send message to output queue
+  boost::system::error_code ec1;
+  serviceSender_->sync_enq(msg,ec1);
+
+  // trigger for receiving messages again
+  serviceReceiver_->async_deq(std::bind(&Service::recvHandler,this,_1,_2));
+}
